b4d6259c1483c46304565ac1827dede4131bd733
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-31 15:29:57 -0500

    Add main component

---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <>
+      <main>
+        <img src='../assets/images/LOGOSIMBOLO.png'/>
+      </main>
+    </>
+  );
+};
+
+export default Main;
