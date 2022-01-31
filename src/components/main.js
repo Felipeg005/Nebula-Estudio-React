bd5dbd124ee1ab96648240ee1f45e0fa7005e809
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-31 15:36:09 -0500

    Import image to component

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../assets/images/LOGOSIMBOLO.png'
 
 const Main = () => {
   return (
     <>
       <main>
-        <img src='../assets/images/LOGOSIMBOLO.png'/>
+        <img className='logo' src={logo}/>
       </main>
     </>
   );
