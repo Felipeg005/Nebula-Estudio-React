cf5858778dce61ac59ee0eb45915c213a0f1a8db
1 <a1@iMac-de-1.local>
2022-03-05 11:41:00 -0500

    Add logo effect when scroll

--- src/components/main.js
+++ src/components/main.js
@@ -12,7 +12,12 @@ const Main = () => {
 };
 
 window.addEventListener('scroll' ,() => {
-  console.log('hi')
+  const logo = document.querySelector('.logo');
+  const scrolled = window.pageYOffset;
+  console.log(window.pageYOffset)
+  const changeVal = scrolled * 0.01;
+  logo.style.transform = `scale(${1 + changeVal})`;
+  logo.style.opacity = `${100 - changeVal * 5}%`;
 })
 
 export default Main;
